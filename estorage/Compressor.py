c50f90bab563fabcca019ada5e69b943898c5272
Jeff Bennett <jab6ft@virginia.edu>
2019-01-26 15:05:46 -0500

    adding more data capture

--- estorage/Compressor.py
+++ estorage/Compressor.py
@@ -1,17 +1,18 @@
 from CoolProp.CoolProp import PropsSI
-from state import def_state_ph, def_state_tp
+from state import def_state_ph, def_state_init
 
 
 class Compressor:
     def __init__(self, eff=80.):
         self.eff = eff  # Isentropic Efficiency (%)
         fluid = 'Air'
-        T = 293.15
-        p = 101325.
-        self.state1 = def_state_tp(fluid, T, p)
-        self.state2 = def_state_tp(fluid, T, p)
+        self.state1 = def_state_init(fluid)
+        self.state2 = def_state_init(fluid)
+        self.m_dot = 0.0
+        self.Q_dot_1 = 0.0
+        self.Q_dot_2 = 0.0
 
-    def compress(self, state1, p2):
+    def compress(self, state1, p2,pwr):
         # Inlet
         fluid = state1['fluid']
         h1 = state1['h']
@@ -22,6 +23,18 @@ class Compressor:
         h2 = h1 + (h2s - h1) / self.eff
         state2 = def_state_ph(fluid,p2,h2)
 
+        # Mass Flow Rate
+        m_dot = pwr/(state2.h-state1.h)
+
+        # Volumetric Flow Rate
+        Q_dot_1 = state1['D']*m_dot
+        Q_dot_2 = state2['D']*m_dot
+
+        # Store results
         self.state1 = state1
         self.state2 = state2
+        self.m_dot = m_dot
+        self.Q_dot_1 = Q_dot_1
+        self.Q_dot_2 = Q_dot_2
+
         return state2
--- estorage/caes.py
+++ estorage/caes.py
@@ -6,11 +6,11 @@ from turbine import Turbine
 
 variables =     ['dt',
               'pwr_i','pwr_o', # Power
-              'T1', 'T2', 'T3', 'T4', 'T5', # Temperature
-              'p1', 'p2', 'p3', 'p4', 'p5', # Pressure
+              'T0', 'T1', 'T2', 'T3', 'T4', 'T5', 'T6', # Temperature
+              'p0', 'p1', 'p2', 'p3', 'p4', 'p5', 'p6', # Pressure
               'm_tank', # Mass stored
-              'm1', 'm2', 'm3', 'm4', 'm5',  # Mass flow
-              'Q1', 'Q2','Q3', 'Q4','Q5'] # Volumetric flow
+              'm0', 'm1', 'm2', 'm3', 'm4', 'm5', 'm6',  # Mass flow
+              'Q0', 'Q1', 'Q2', 'Q3', 'Q4', 'Q5','Q6'] # Volumetric flow
 
 class CAES:
     def __init__(self,V=1000.,cmp_eff=80.,trb_eff=90.,p=200000):
@@ -37,44 +37,70 @@ class CAES:
             self.discharge(pwr, dt)
         # Do nothing
         else:
+            self.maintain(dt)
 
         # Store current state
         s = pd.Series(index=variables)
         s['dt'] = dt
         s['pwr'] = pwr
-        s['T1'] = self.amb.state
-        s['T2'] = self.cmp.state2
-        s['T3'] =
-        s['T4'] =
-        s['T5'] =
-        s['p1'] = self.amb.state
-        s['p2'] = self.cmp.state2
-        s['p3'] =
-        s['p4'] =
-        s['p5'] =
-        s['m_tank'] =
-        s['m1'] = self.cmp.m_dot
-        s['m2'] = self.cmp.m_dot
-        s['m3'] =
-        s['m4'] =
-        s['m5'] =
-        s['Q1'] = self.cmp.Q_dot_1
+
+        # -----
+        # Store Tank Data
+        #-----
+            # Temperature
+        s['T_TA'] = 0.0
+        s['T_TC'] = 0.0
+        s['T_TH'] = 0.0
+            # Pressure
+        s['p_TA'] = 0.0
+        s['p_TC'] = 0.0
+        s['p_TH'] = 0.0
+            # Mass stored
+        s['m_TA'] = self.tank.m
+        s['m_TC'] = 0.0
+        s['m_TH'] = 0.0
+
+        # -----
+        # Store Flow Data
+        # -----
+            # Temperature
+        s['T_A1'] = self.cmp.state1['T']
+        s['T_A2'] = self.cmp.state2['T']
+        s['T_A3'] = 0.0
+        s['T_B1'] = 0.0
+        s['T_B2'] = 0.0
+        s['T_B3'] = 0.0
+        s['T_C1'] = 0.0
+        s['T_C2'] = 0.0
+        s['T_D1'] = 0.0
+        s['T_D2'] = 0.0
+            # Pressure
+        s['p_A1'] = self.cmp.state1['p']
+        s['p_A2'] = self.cmp.state2['p']
+        s['p_A3'] = self.tank.state['p']
+        s['p_B1'] = 0.0
+        s['p_B2'] = 0.0
+        s['p_B3'] = 0.0
+        s['p_C1'] = 0.0
+        s['p_C2'] = 0.0
+        s['p_D1'] = 0.0
+        s['p_D2'] = 0.0
+        s['p_E1'] = 0.0
+        s['p_E2'] = 0.0
+            # Mass flow rate
+        s['m_dot_A'] = self.cmp.m_dot
+        s['m_dot_B'] = self.cmp.m_dot
+        s['m_dot_C'] = self.cmp.m_dot
+        s['m_dot_D'] = self.cmp.m_dot
+        s['m_dot_E'] = self.cmp.m_dot
+            # Volumetric flow rate
+        s['Q_A1'] = self.cmp.Q_dot_1
         s['Q2'] = self.cmp.Q_dot_2
-        s['Q3'] =
-        s['Q4'] = 0.0
-        s['Q5'] = 0.0
-
+        s['Q3'] = self.tank.state['T']
+        s['Q4'] = self.trb.Q_dot_1
+        s['Q5'] = self.trb.Q_dot_2
+        # Add to DataFrame
         self.df.append(s)
-
-
-
-        state1 = self.amb.state
-        p_tank = self.tank.state.p
-        state2 = self.cmp.compress(state1, p_tank,pwr)
-        m_dot_i = self.cmp.m_dot
-        state3 = state2 # Hxer - to be added
-        self.tank.charge(state3,m_dot_i,dt)
-        self.m_dot_i = m_dot_i
 
     def charge(self, pwr, dt):
         # Do something
@@ -94,4 +120,7 @@ class CAES:
         state6 = self.trb.expand(state4, p_amb)
         m_dot = pwr / (state4.h - state5.h)
         self.tank.discharge(state4, m_dot, dt)
+
+    def maintain(self, dt):
+        # Turn Everything off
 
