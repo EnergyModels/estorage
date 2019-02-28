3abd82906f6e57c89e85efb317ae0da7173ed287
EnergyModels <jab6ft@virginia.edu>
2019-02-28 11:13:34 -0500

    moved to synthetic oil

--- estorage/compressor_sizing.py
+++ estorage/compressor_sizing.py
@@ -12,9 +12,12 @@ PR_stg_min = 1.5
 PR_stg_max = 3.6
 
 # Specific Speed Chart Inputs
-Ns_ideal = np.array([17.53573098, 20.31460093, 23.85633867, 29.94568557, 39.64060977, 60.83324952, 139.5346754])
-Ds_ideal = np.array([7.848168406, 6.761801714, 5.905258342, 4.690713434, 3.388928695, 2.288095758, 1.116116401])
-eff_ideal = np.array([0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.8])
+
+Ns_ideal = np.array([0.156592415,0.213784951,0.314421351,0.374907082,0.462458607,0.627128919,1.063672257,2.33449929,6.27822242,8.458415169,11.24347938,16.65613494,26.4048703])
+Ds_ideal = np.array([17.82794648,12.62762237,8.191486162,6.642324943,5.533819167,4.054427281,3.03140973,2.491585363,1.850350655,1.616281072,1.520845751,1.337472543,1.168282103])
+eff_ideal = np.array([0.5,0.6,0.7,0.75,0.8,0.85,0.85,0.85,0.8,0.75,0.7,0.6,0.5])
+Ns_radial = [0.156, 0.627]
+Ns_axial = [2.334, 26.405]
 
 
 def SIZE_AIR_CMP(p_in=1.01325, t_in=20.0, p_out=10.0, m_dot=2.2, RPM_low=10000, RPM_high=50000, RPM_cases = 5, debug=False):
@@ -32,10 +35,13 @@ def SIZE_AIR_CMP(p_in=1.01325, t_in=20.0, p_out=10.0, m_dot=2.2, RPM_low=10000, 
     Nstg_low = math.ceil(math.log(PR) / math.log(PR_stg_max))
     Nstg_high = math.floor(math.log(PR) / math.log(PR_stg_min))
     Nstgs = np.arange(Nstg_low, Nstg_high, 1)
+    if len(Nstgs)==0:
+            Nstgs = [Nstg_low]
     if debug == True:
+        print 'Range of Stages Considered'
         print 'Nstg_low  :' + str(round(Nstg_low, 0))
         print 'Nstg_high :' + str(round(Nstg_high, 0))
-        print str(Nstgs)
+        print 'Nstgs     :' + str(Nstgs) + '\n'
 
     # RPMs to consider
     RPMs = np.linspace(RPM_low, RPM_high, RPM_cases)
@@ -43,17 +49,32 @@ def SIZE_AIR_CMP(p_in=1.01325, t_in=20.0, p_out=10.0, m_dot=2.2, RPM_low=10000, 
     # Constants and Fluid Properties
     g = 9.81  # m/s^2
     fluid = 'Air'
-    CP = PropsSI('CPMASS', "T", t_in, "P", p_in, fluid) * 1000.0  # KJ/Kg
-    CV = PropsSI('CVMASS', "T", t_in, "P", p_in, fluid) * 1000.0  # KJ/Kg
+    CP = PropsSI('CPMASS', "T", t_in, "P", p_in, fluid) / 1000.0  # KJ/Kg-K
+    CV = PropsSI('CVMASS', "T", t_in, "P", p_in, fluid) / 1000.0  # KJ/Kg-K
     kappa = CP / CV
     MW = PropsSI('M', fluid) * 1000.0  # kg/kmol
     R_bar = PropsSI('GAS_CONSTANT', fluid)  # kJ/kmol/K
     R = R_bar / MW * 1000.0  # J/kg-K
-    D1 = PropsSI('H', 'T', t_in, 'P', p_in, fluid)  # Density (kg/m3)
+    D1 = PropsSI('D', 'T', t_in, 'P', p_in, fluid)  # Density (kg/m3)
     V1 = m_dot * D1 # m3/s
 
+    # Print-out values, if debugging
+    if debug == True:
+        print 'Constants and Fluid Properties:'
+        print 'g     :' + str(round(g, 3)) + ' (m/s^2)'
+        print 'CP    :' + str(round(CP, 3)) + ' (kJ/kg-K)'
+        print 'CV    :' + str(round(CV, 3)) + ' (kJ/kg-K)'
+        print 'kappa :' + str(round(kappa, 3)) + ' (-)'
+        print 'MW    :' + str(round(MW, 3)) + ' (kg/kmol)'
+        print 'R_bar :' + str(round(R_bar, 3)) + ' (kJ/kmol-K)'
+        print 'R     :' + str(round(R, 3)) + ' (J/kg-K)'
+        print 'D1    :' + str(round(D1, 3)) + ' (kg/m^3)'
+        print 'V1    :' + str(round(V1, 3)) + ' (m^3/s)\n'
+        print 'Begin Cases'
+
     # DataFrame to hold results
-    variables = ['p_in', 't_in', 'p_out', 'm_dot', 'V1', 'Nstg', 'PR_stg', 'RPM', 'Ns', 'Ds', 'D', 'eff']
+    variables = ['p_in', 't_in', 'p_out', 'm_dot', 'V1', 'Nstg', 'PR_stg', 'RPM', 'H_ad',
+                 'g', 'Ns', 'Ds', 'D', 'eff', 'type']
     df = pd.DataFrame(columns=variables)
 
     # Perform Runs
@@ -65,40 +86,53 @@ def SIZE_AIR_CMP(p_in=1.01325, t_in=20.0, p_out=10.0, m_dot=2.2, RPM_low=10000, 
 
             # Balje Calculations (Ideal gas)
             omega = 2 * pi / 60.0 * RPM  # rad/s
-            omega = RPM
+            # omega = RPM
             H_ad = kappa / (kappa - 1.0) * R * t_in * ((PR_stg) ** ((kappa - 1.0) / kappa) - 1.0)  # kJ/kg
             Ns = (omega*V1**0.5)/(H_ad)**0.75
 
             # Print-out values, if debugging
             if debug == True:
-                print 'Nstg  :' + str(round(Nstg,0))
-                print 'PR_stg:' + str(round(PR_stg,2))
-                print 'RPM   :' + str(round(RPM,0))
-                print 'Ns    :' + str(round(Ns,2)) + "\n"
+                print 'Nstg  :' + str(round(Nstg,0)) + ' (-)'
+                print 'PR_stg:' + str(round(PR_stg,2)) + ' (-)'
+                print 'RPM   :' + str(round(RPM,0)) + ' (rev/min)'
+                print 'omega :' + str(round(omega,2)) +' (rad/s)'
+                print 'H_ad  :' + str(round(H_ad,2)) + ' (kJ/kg)'
+                print 'Ns    :' + str(round(Ns,3)) + " (-)\n"
 
             # Check if within the interpolation limits
             if Ns_ideal.min() <= Ns and Ns <= Ns_ideal.max():
                 eff = f_eff(Ns)
                 Ds =  f_Ds(Ns)
-                D = Ds * V1 ** 0.5 / (g * H_ad) ** 0.75
+                D = (Ds * (V1) ** 0.5) / (g * H_ad) ** 0.25
+
+                # Classify Machine Type
+                if Ns < Ns_radial[1]:
+                    machine_type = 'Radial'
+                elif Ns_axial[0] < Ns:
+                    machine_type = 'Axial'
+                else:
+                    machine_type = 'Mixed'
 
                 # Print-out values, if debugging
                 if debug == True:
                     print "Successfully sized"
-                    print 'Ds    :' + str(round(Ds,2))
-                    print 'D     :' + str(round(D,2))
-                    print 'eff   :' + str(round(eff,2))
+                    print 'Ds    :' + str(round(Ds,3))
+                    print 'D     :' + str(round(D,3))
+                    print 'eff   :' + str(round(eff,3))
                     print '#================#\n'
 
                 # Save Values
-                s = pd.Series(index=['Nstg', 'PR_stg', 'RPM', 'Ns', 'Ds', 'D', 'eff'])
+                s = pd.Series(index=['Nstg', 'PR_stg', 'RPM', 'H_ad', 'g', 'Ns', 'Ds', 'D', 'eff', 'type'])
                 s['Nstg'] = Nstg
                 s['PR_stg'] = PR_stg
                 s['RPM'] = RPM
+                s['H_ad'] = H_ad
+                s['g'] = g
                 s['Ns'] = Ns
                 s['Ds'] = Ds
-                s['D'] = 0.0
-                s['eff'] = 0.0
+                s['D'] = D
+                s['eff'] = eff
+                s['type'] = machine_type
                 df = df.append(s, ignore_index=True)
 
     # Store Inputs
--- examples/1D_sizing/size_compressor.py
+++ examples/1D_sizing/size_compressor.py
@@ -1,16 +1,37 @@
 from estorage import SIZE_AIR_CMP
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 
 # Test
 designs = SIZE_AIR_CMP(p_in=1.01325, t_in=20.0, p_out=2.2, m_dot=2.2, RPM_low=22000, RPM_high=22000, RPM_cases = 1, debug=True)
 
 # Run Sweep
-designs = SIZE_AIR_CMP(p_in=1.01325, t_in=20.0, p_out=31.1, m_dot=13.82, RPM_low=3600, RPM_high=50000, RPM_cases = 5, debug=False)
+designs = SIZE_AIR_CMP(p_in=1.01325, t_in=20.0, p_out=31.1, m_dot=13.82, RPM_low=1800, RPM_high=50000, RPM_cases = 20, debug=False)
+
+designs.to_csv("cmp_sizing_results.csv")
 
 # Plot Results
 if len(designs)>0:
     sns.set_style('white')
+
+    # Plot 2 - Ns and Ds
     f,a = plt.subplots(2,1,sharex=True)
-    sns.lineplot(x='RPM',y='eff',hue='Nstg',data=designs,ax=a[0])
-    sns.lineplot(x='RPM',y='D',hue='Nstg',data=designs,ax=a[1])
-    f.savefig('cmp_sizing.png',dpi=1000)
+    sns.lineplot(x='RPM', y='Ns', hue='Nstg', data=designs, ax=a[0])
+    sns.lineplot(x='RPM', y='Ds', hue='Nstg', data=designs, ax=a[1])
+    f.savefig('cmp_sizing_NsDs.png',dpi=1000)
+
+    # Plot 2 - D and Eff
+    # f, a = plt.subplots(2, 1, sharex=True)
+    # sns.lineplot(x='RPM', y='D', hue='Nstg', data=designs, ax=a[0])
+    # sns.lineplot(x='RPM', y='eff', hue='Nstg', data=designs, ax=a[1])
+
+    sns.PairGrid(designs, x_vars=['RPM'], y_vars=['eff','D'], hue='Nstg')
+    # sns.lineplot(x='RPM', y='eff', hue='Nstg', data=designs, ax=a[1])
+    f.savefig('cmp_sizing_Deff.png', dpi=1000)
+
+    ind = designs.loc[:,type]=='Radial' or designs.loc[:,'type']=='Mixed'
+    designs2 = designs.loc[ind, :]
+    sns.PairGrid(designs2, x_vars=['RPM'], y_vars=['eff', 'D'], hue='Nstg')
+    # sns.lineplot(x='RPM', y='eff', hue='Nstg', data=designs, ax=a[1])
+    f.savefig('cmp_sizing_Deff_radialMixed.png', dpi=1000)
